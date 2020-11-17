4e51bb51660590bb59da3113bb5c6d4793e1cd68
Harshit Singhai <harshitsinghai77@gmail.com>
2020-11-17 15:38:37 +0530

    doing it the right way

--- node/script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-node ./app.js&
-node ./worker.js&
-
-wait
--- python/script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-flask run --host=0.0.0.0 --port=8003&
-celery -A src.worker:celery worker --loglevel=INFO&
-
-wait
